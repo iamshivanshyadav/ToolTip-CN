c369bd9d71d7d68c3f5442ac0f1a2a530f156014
Shivansh Yadav <shivanshyadav50@gmail.com>
2023-08-26 16:54:14 +0530

    Update MuiTooltip.tsx

--- components/MuiTooltip.tsx
+++ components/MuiTooltip.tsx
@@ -1,6 +1,6 @@
 
 import { Tooltip, IconButton } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/delete"
+import DeleteIcon from '@mui/icons-material/Delete';
 export const MuiTooltip = () => {
     return (
       <Tooltip title='Delete' placement="right">
